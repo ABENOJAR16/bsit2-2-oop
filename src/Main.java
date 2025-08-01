fa3e9dc57aa37a49d9c3639f047462e48d08d6ba
ABENOJAR16 <znabenojar88754@liceo.edu.ph>
2025-08-01 10:06:28 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+import java.util.Scanner;
+
+public class main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner malongScanner = new Scanner(System.in);
+
+        int totalNumber = 0;
+        int number;
+
+        for (int i = 1; i <= 5 ; i++){
+            System.out.println("enter number " +i + ": ");
+            number = malongScanner.nextInt();
+            totalNumber += number;
         }
 
+        System.out.println(" ");
+        System.out.println("total number is " + totalNumber);
+
+
+    }
 }
+
+
